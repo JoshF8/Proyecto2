bc1f17279e26b1606742f8d8bbeee0d9f3988c49
JoshF8 <joshuafranco98@gmail.com>
2018-05-10 17:24:50 -0600

    Inicio

---
+++ Mundial/src/Listas/ListaVentanas.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Listas;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Josh
+ */
+public class ListaVentanas {
+    private JFrame ventanaInicial;
+    
+    public ListaVentanas(JFrame ventana) {
+        ventanaInicial = ventana;
+    }
+    
+    public void crearVentana(int valor){
+        switch(valor){
+            
+        }
+    }
+}
